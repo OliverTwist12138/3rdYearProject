ef6f34fdd8f1298b33084908e97a720ebb782807
Zhihan <zhihanyan12138@gmail.com>
2020-09-30 01:05:04 +0800

    combine 20 2d images to 3d

--- dcmtest.py
+++ dcmtest.py
@@ -1,25 +1,31 @@
 from pydicom import dcmread
 import os
+import numpy as np
 
 cnt = 0
+def make3D(ds):
+    arr = ds.pixel_array
+
+def enterfile(path):
+    return path + "/" + os.listdir(path)[0]
+
 def readFile(path):
     files = os.listdir(path)
-    dsCube = []
+    ds_list = []
     for file in files:
         filePath = path + "/" + file
-        if os.path.isdir(filePath):
-            readFile(filePath)
-        else:
-            ds = dcmread(filePath)
-            global cnt
-            print("This is the {0} file".format(cnt),end="\r")
-            cnt += 1
-            dsCube.append(ds)
-        if len(dsCube) == 20:
-            break
-    return dsCube
+        while os.path.isdir(filePath):
+            filePath = enterfile(filePath)
+        ds = dcmread(filePath)
+        global cnt
+        #print("This is the {0} file".format(cnt),end="\r")
+        cnt += 1
+        ds_list.append(list(ds.pixel_array))
+        if cnt == 20:
+            return np.array(ds_list)
+
 def main():
-    path = "C:/Users/57035/Desktop/ELEC/3rd year/RSNA Chest Xray/images in initial annotation"
+    path = "C:/Users/57035/Desktop/ELEC/3rd year/project/Xray/images in initial annotation"
     dsList = readFile(path)
     print(dsList)
 if __name__ == "__main__":
--- iotest.py
+++ iotest.py
@@ -10,7 +10,7 @@ def extractInfo(ds):
     info["StudyTime"] = ds.StudyTime
     return info
 def main():
-    path = "C:/Users/57035/Desktop/ELEC/3rd year/RSNA Chest Xray/images in initial annotation/1.2.276.0.7230010.3.1.2.8323329.10002.1517874346.165591/1.2.276.0.7230010.3.1.3.8323329.10002.1517874346.165590/1.2.276.0.7230010.3.1.4.8323329.10002.1517874346.165592.dcm"
+    path = "C:/Users/57035/Desktop/ELEC/3rd year/project/RSNA Chest Xray/images in initial annotation/1.2.276.0.7230010.3.1.2.8323329.1472.1517874291.114974/1.2.276.0.7230010.3.1.3.8323329.1472.1517874291.114973/1.2.276.0.7230010.3.1.4.8323329.1472.1517874291.114975.dcm"
     ds = dcmread(path)
     '''
     print(ds)
